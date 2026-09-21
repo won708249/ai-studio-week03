3a0d113eff9caac4cc3df0e17bfa9b9c64e140fd
Jeong Jiwon <jjw1167@sogang.ac.kr>
2026-09-21 23:49:05 +0900

    feat: aggregate data with groupby

--- pipeline.py
+++ pipeline.py
@@ -14,3 +14,11 @@ df["매출액"] = df["단가"] * df["수량"]
 df["주문일자"] = pd.to_datetime(df["주문일자"])
 
 df["월"] = df["주문일자"].dt.month
+
+result = df.groupby(["월", "카테고리"]).agg(
+    매출액_총합=("매출액", "sum"),
+    매출액_평균=("매출액", "mean"),
+    거래건수=("매출액", "count")
+).reset_index()
+print(result)
+
